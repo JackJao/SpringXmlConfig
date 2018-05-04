4c13c6c554d3f51cf44d4d159203afdbedb879a6
JackJao <766056142@qq.com>
2018-05-04 18:13:45 +0800

    1.注释说明java中使用xml中bean的配置@ImportResource({"配置文件所在路径"}) 2.注释说明xml使用java配置的bean，使用<bean class="配置类"> 3.两个都可以提取功能配置来整合，如职责分离的配置整合一起

--- src/main/java/com/jason/demo/java2xml/JavaConfigCommon.java
+++ src/main/java/com/jason/demo/java2xml/JavaConfigCommon.java
@@ -2,12 +2,25 @@ package com.jason.demo.java2xml;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * 把分开的配置整合在一起,纯java方式,演示配置一分为二，合二为一的方式
  * 这里的import没有顺序区分
  */
 @Configuration
+//把2个spring的java配置整合在一起
 @Import(value = {JavaConfig1.class,JavaConfig2.class})
 public class JavaConfigCommon {
 }
+
+/**
+ * 一、把spring的java配置和xml配置合在一起使用
+ */
+@Configuration
+@Import(JavaConfig1.class)
+@ImportResource({"classpath:applicationContext.xml"})
+/*public */class JavaConfigCommon2 {
+}
+
+
