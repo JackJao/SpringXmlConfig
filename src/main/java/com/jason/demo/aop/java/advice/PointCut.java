1d8d5f0f00548643ca6d21d681c33ce77ff79953
JackJao <766056142@qq.com>
2018-05-16 18:23:34 +0800

    使用 aop 增加一个类的方法和属性

--- src/main/java/com/jason/demo/aop/java/advice/PointCut.java
+++ src/main/java/com/jason/demo/aop/java/advice/PointCut.java
@@ -11,6 +11,16 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class PointCut {
 
+    /**
+     * @DeclareParents注解由三部分组成：
+    value属性指定了哪种类型的bean要引入该接口。在本例中，也就是所有实现
+    Animal的类型。（标记符后面的加号表示是Animal的所有子类型，而
+    不是Animal本身。）
+    defaultImpl属性指定了为引入功能提供实现的类。在这里，我们指定的
+    是addInteface提供实现。
+     @DeclareParents注解所标注的静态属性指明了要引入了接口。在这里，我们所引入
+     的是Encoreable接口。
+     */
     @DeclareParents(value = "com.jason.demo.aop.java.advice.Animal+",defaultImpl = AddIntefaceImpl.class)
     private AddInteface addInteface;
 }
